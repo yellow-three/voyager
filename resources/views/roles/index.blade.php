e06a4055ea9702d54b212e658ea8eda8842a72b3
Abdurrahman Harmankaya <abdurrahman.kim@gmail.com>
2026-05-23 23:44:34 +0300

    feat(role-user-compass): implement Livewire 4 SFCs for Role, User, and Compass

---
+++ resources/views/roles/index.blade.php
@@ -0,0 +1,7 @@
+@extends('voyager::master')
+
+@section('page_title', __('voyager::generic.roles'))
+
+@section('content')
+    <livewire:voyager::⚡role-list />
+@stop
---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,7 @@
+@extends('voyager::master')
+
+@section('page_title', __('voyager::generic.users'))
+
+@section('content')
+    <livewire:voyager::⚡user-list />
+@stop
