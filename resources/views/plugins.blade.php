fd4e20f34cd012ec54a5f26e83dee98aa688c166
Abdurrahman Harmankaya <abdurrahman.kim@gmail.com>
2026-05-23 23:50:57 +0300

    feat(plugin-theme-mfc): implement Livewire 4 Plugins and Themes Manager MFCs

---
+++ resources/views/plugins.blade.php
@@ -0,0 +1,7 @@
+@extends('voyager::master')
+
+@section('page_title', 'Plugins Manager')
+
+@section('content')
+    <livewire:voyager::⚡plugins-manager />
+@stop
---
+++ resources/views/themes.blade.php
@@ -0,0 +1,7 @@
+@extends('voyager::master')
+
+@section('page_title', 'Themes Manager')
+
+@section('content')
+    <livewire:voyager::⚡themes-manager />
+@stop
